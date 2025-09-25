a7afc33fda76dfb6e8c1b936b3e51cdefe69ab36
NathanFlurry <git@nathanflurry.com>
2025-09-25 23:13:58 +0000

    chore(docker): add default token to dev docker (#2977)

--- docker/template/src/services/edge/rivet-engine.ts
+++ docker/template/src/services/edge/rivet-engine.ts
@@ -8,7 +8,8 @@ export function generateDatacenterRivetEngine(
 	context: TemplateContext,
 	datacenter: Datacenter,
 ) {
-	const clickhouseHost = context.config.networkMode === "host" ? "127.0.0.1" : "clickhouse";
+	const clickhouseHost =
+		context.config.networkMode === "host" ? "127.0.0.1" : "clickhouse";
 	const datacenters = [];
 
 	for (let dc of context.config.datacenters) {
@@ -24,7 +25,11 @@ export function generateDatacenterRivetEngine(
 
 	// Generate a separate config file for each engine node
 	for (let i = 0; i < datacenter.engines; i++) {
-		let serviceHost = context.getServiceHost("rivet-engine", datacenter.name, 0);
+		let serviceHost = context.getServiceHost(
+			"rivet-engine",
+			datacenter.name,
+			0,
+		);
 		const topology = {
 			datacenter_label: datacenter.id,
 			datacenters,
@@ -32,31 +37,24 @@ export function generateDatacenterRivetEngine(
 
 		// Config structure matching Rust schema in packages/common/config/src/config/mod.rs
 		const config = {
-			// guard config
+			auth: {
+				admin_token: "dev",
+			},
 			guard: {
 				port: GUARD_PORT,
 				// https is optional and not configured for local development
 			},
-			// api_peer config
 			api_peer: {
 				host: "0.0.0.0",
 				port: API_PEER_PORT,
 			},
-			// logs config
-			logs: {
-				// redirect_logs_dir is optional
-			},
-			// topology config
 			topology,
-			// database & pubsub config
 			postgres: {
 				url: `postgresql://postgres:postgres@${context.getServiceHost("postgres", datacenter.name)}:5432/rivet_engine`,
 			},
-			// cache config
 			cache: {
 				driver: "in_memory",
 			},
-			// clickhouse config (optional)
 			clickhouse: {
 				http_url: `http://${clickhouseHost}:9300`, // TODO:
 				native_url: `http://${clickhouseHost}:9301`, // TODO:
@@ -72,7 +70,6 @@ export function generateDatacenterRivetEngine(
 				},
 				secure: false,
 			},
-			// vector_http config (optional)
 			vector_http: {
 				host: context.getServiceHost("vector-client", datacenter.name),
 				port: 5022,
